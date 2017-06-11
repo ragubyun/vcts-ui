3cc6e2ed9ed6950b137b031723f3dc5a9b3f8295
devori.lee <devori.lee@gmail.com>
2017-06-11 16:13:04 +0900

    apply vuex

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    {{ msg }}
+  </div>
+</template>
+
+<script>
+import store from '@/store'
+export default {
+  store,
+  name: 'hello',
+  computed: {
+    msg () {
+      return this.$store.state.hello
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
